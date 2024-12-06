cb1b18640a717614769dda754d891e0955767d69
27jwong <guineapig9@gmail.com>
2024-12-05 21:42:08 -0500

    Update screenrecorder.h

--- selfdrive/frogpilot/screenrecorder/screenrecorder.h
+++ selfdrive/frogpilot/screenrecorder/screenrecorder.h
@@ -6,13 +6,20 @@
 #include "selfdrive/ui/qt/onroad/buttons.h"
 
 class ScreenRecorder : public QPushButton {
+#ifdef NO_SR
+  public:
+    explicit ScreenRecorder(QWidget *parent = nullptr){}
+    ~ScreenRecorder() override{}
+
+    void updateScreen(){}
+#else
   Q_OBJECT
 
 public:
   explicit ScreenRecorder(QWidget *parent = 0);
   ~ScreenRecorder() override;
 
-  void updateScreen(double fps, bool started);
+  void updateScreen();
 
 private:
   void closeEncoder();
@@ -25,10 +32,6 @@ private:
 
   BlockingQueue<QImage> imageQueue{UI_FREQ};
   BlockingQueue<QImage> encodeQueue{UI_FREQ};
-
-  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
-  inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
-  inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
 
   QWidget *rootWidget;
 
@@ -43,5 +46,6 @@ private:
 
   std::unique_ptr<OmxEncoder> encoder;
 
-  std::unique_ptr<uint8_t[]> rgbScaleBuffer;
+  std::vector<uint8_t> rgbScaleBuffer;
+#endif
 };
