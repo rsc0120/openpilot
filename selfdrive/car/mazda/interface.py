13a8ffbb4362a857a786f8c30f64532ee399880a
MoreTorque <104254025+MoreTore@users.noreply.github.com>
2024-12-17 06:23:57 -0600

    Manual Transmission

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -21,9 +21,9 @@ class CarInterface(CarInterfaceBase):
 
     ret.dashcamOnly = False
     ret.openpilotLongitudinalControl = True
+    p = Params()
     if candidate in GEN1:
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_MAZDA_GEN1
-      p = Params()
       if p.get_bool("TorqueInterceptorEnabled"): # Torque Interceptor Installed
         print("Torque Interceptor Installed")
         ret.flags |= MazdaFlags.TORQUE_INTERCEPTOR.value
