2d18c58c84151260e273e0decd6b2264c8abed22
rsc0120 <117456004+rsc0120@users.noreply.github.com>
2025-05-17 11:17:51 +0800

    Revert "No FSC"
    This reverts commit b587098c57383f0035f954ccc955aa5496cd3dca.

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -74,8 +74,7 @@ class CarController(CarControllerBase):
         steer_required = CC.hudControl.visualAlert == VisualAlert.steerRequired
         # TODO: find a way to silence audible warnings so we can add more hud alerts
         steer_required = steer_required and CS.lkas_allowed_speed
-        if not self.CP.flags & MazdaFlags.NO_FSC:
-          can_sends.append(mazdacan.create_alert_command(self.packer, CS.cam_laneinfo, ldw, steer_required))
+        can_sends.append(mazdacan.create_alert_command(self.packer, CS.cam_laneinfo, ldw, steer_required))
 
       if self.CP.flags & MazdaFlags.RADAR_INTERCEPTOR:
         hold = False
